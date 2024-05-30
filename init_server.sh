2ab9b92353f1fb6088eee98f32c23c33bfabe59d
user.name <marcorandazzo051291@gmail.com>
2024-05-31 00:26:54 +0200

    clean code

--- init_server.sh
+++ init_server.sh
@@ -24,9 +24,3 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 newgrp docker
-
-cd $HOME/app/Ludwig
-
-docker build -t ludwig:latest . --no-cache
-
-docker run -d -p 443:443 -v $HOME/app/Ludwig/public:/app/public ludwig:latest
