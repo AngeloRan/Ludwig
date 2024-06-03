11f071e244470b4e488ee7a628e979477f684f64
user.name <marcorandazzo051291@gmail.com>
2024-06-03 19:39:17 +0200

    entrypoint to server r-icons

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,20 @@ if [ "$1" == 'start' ]; then
     export ORIGIN="$ORIGIN" 
     
     cd /app
+    rm -rf dist
     npm i
+    npm install react-icons
     npm run build
     npm install -g serve
-    serve -s dist &
+    
 
     if [ "$#" -ge 3 ]; then
+        serve -s dist &
         DOMAIN="$3"
         sed -i "s/SERVERNAME/$DOMAIN/g" /etc/nginx/nginx.conf && \
         sleep 3 && service nginx start & \
+    else
+        serve -s dist
     fi
 fi
 
